7f21e52f6761dc7ec23203de8bbed63655531670
shareef-sanaullah <57405273+shareef-sanaullah@users.noreply.github.com>
2019-11-06 22:02:37 +0300

    Add files via upload

---
+++ Batch6_Python_Day2.py
@@ -0,0 +1,93 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+first_name = 'shareef'
+last_name = 's'
+
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+
+# In[3]:
+
+
+print(full_name.title())
+
+
+# In[4]:
+
+
+print(' python')
+
+
+# In[5]:
+
+
+print('\tpython')
+
+
+# In[6]:
+
+
+print('Languages:\npython\nc\njava')
+
+
+# In[7]:
+
+
+print('Languages:\n\tpython\n\tc\n\tjava')
+
+
+# In[8]:
+
+
+fav_lang = 'english'
+print(fav_lang)
+
+
+# In[9]:
+
+
+fav_lang = 'english '
+print(fav_lang)
+
+
+# In[10]:
+
+
+fav_lang.rstrip()
+
+
+# In[11]:
+
+
+fav_lang1 = ' french'
+print(fav_lang1)
+
+
+# In[12]:
+
+
+fav_lang1.lstrip()
+
+
+# In[13]:
+
+
+fav_lang1.strip
+
+
+# In[14]:
+
+
+fav_lang1.strip()
+
+
+# In[ ]:
+
+
+
+
